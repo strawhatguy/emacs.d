0b663ea9f86cc6d04ed2ddd09fec0a9732f170c9
Matthew Curry <mjcurry@gmail.com>
2015-02-14 12:58:34 -0500

    Add ability to wrap js code into functions

--- package-init-files/init-js2-mode.el
+++ package-init-files/init-js2-mode.el
@@ -13,3 +13,34 @@
     (interactive) 
     (js2-end-of-defun)))
 (define-key js2-mode-map (kbd "C-c m") 'js2-mark-defun)
+
+(eval-after-load 'js2-mode
+  '(progn
+     (defun wrap-in-js-function ()
+       (interactive)
+       (save-excursion
+         (unless (use-region-p)
+           (push-mark)
+           (end-of-line))
+         (save-restriction
+           (narrow-to-region (region-beginning) (region-end))
+           (goto-char (point-min))
+           (insert "function (arg) {")
+           (goto-char (point-max))
+           (insert "}")
+           (goto-char (point-min))
+           (search-forward "(arg)" nil t))))
+
+     (defun unwrap-js-function ()
+       (interactive)
+       (save-excursion
+         (js2-mark-defun)
+         (save-restriction
+           (narrow-to-region (region-beginning) (region-end))
+           (goto-char (point-min))
+           (zap-to-char 1 ?\{)
+           (goto-char (point-max))
+           (zap-to-char -1 ?\}))))
+
+     (define-key js2-mode-map (kbd "C-c w f") 'wrap-in-js-function)
+     (define-key js2-mode-map (kbd "C-c u f") 'unwrap-js-function)))
