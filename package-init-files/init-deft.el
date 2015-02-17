d77748d9bb46b7f286936e9646c0721ce7bb7a62
mcurry <matthew.curry@adp.com>
2015-02-17 17:57:42 -0500

    Add elfeed, js2-refactor, change deft directory

---
+++ package-init-files/init-deft.el
@@ -0,0 +1,4 @@
+(setq deft-directory "~/.emacs.d/.deft")
+(setq deft-extension "org")
+(setq deft-text-mode 'org-mode)
+(global-set-key [f7] 'deft)
---
+++ package-init-files/init-elfeed.el
@@ -0,0 +1,6 @@
+(global-set-key (kbd "C-x w") 'elfeed)
+(setq elfeed-feeds
+      '("http://nullprogram.com/feed/"
+        "http://www.terminally-incoherent.com/blog/feed/"
+        "http://planet.lisp.org/rss20.xml"
+        "http://emacsredux.com/atom.xml"))
