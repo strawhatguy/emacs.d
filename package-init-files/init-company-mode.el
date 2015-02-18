194c81e9d653cb83469fa8a0eb9ed5905c4b5402
mcurry <matthew.curry@adp.com>
2015-02-17 22:51:52 -0500

    Tweak the keys for company mode a bit

--- package-init-files/init-company-mode.el
+++ package-init-files/init-company-mode.el
@@ -1,2 +1,5 @@
 (setq company-idle-delay 0.1)
+(define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
 (add-hook 'after-init-hook 'global-company-mode)
