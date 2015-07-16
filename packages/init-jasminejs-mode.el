9e55108549c43eeaa42236dd4a1cef5358980b5d
Matthew Curry <matthew.curry@adp.com>
2015-07-16 14:55:23 -0400

    get jasminejs from melpa, add bookmark+

--- packages/init-jasminejs-mode.el
+++ packages/init-jasminejs-mode.el
@@ -1,7 +1,6 @@
 (add-hook 'jasminejs-mode-hook
           (lambda ()
-            (jasminejs-add-snippets-to-yas-snippet-dirs (concat user-emacs-directory
-                                                                "el-get/jasminejs-mode/snippets/"))
+            (jasminejs-add-snippets-to-yas-snippet-dirs)
             (local-set-key (kbd "C-c j") 'jasminejs-prefix-map)))
 
 (add-hook 'js2-mode-hook 'jasminejs-mode)
