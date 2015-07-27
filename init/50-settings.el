dc6fbb5e626272c3ad5459305a6005df84d8ba93
Matthew Curry <matthew.curry@adp.com>
2015-07-27 12:56:40 -0400

    Better mouse scrolling

--- init/50-settings.el
+++ init/50-settings.el
@@ -45,3 +45,9 @@
 
 ;;;; company mode messes up eshell
 (add-hook 'eshell-mode-hook (lambda () (company-mode -1)))
+
+;;;; try and make mouse less jerky
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil)            ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't)                  ;; scroll window under mouse
+(setq scroll-step 1)                                ;; keyboard scroll one line at a time
