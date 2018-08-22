f14af937a1fce2fba0da88a61620e4f59cbfb049
Matthew Curry <mcurr4@c02wv08ahtd8.ads.aexp.com>
2018-08-21 23:03:45 -0400

    new emacs and package updates

--- init.el
+++ init.el
@@ -38,9 +38,6 @@
   :config
   (setq avy-background t))
 
-(use-package bookmark+
-  :ensure t)
-
 (use-package cargo
   :ensure t)
 
@@ -357,7 +354,7 @@
 (use-package projectile
   :ensure t
   :config
-  (projectile-global-mode)
+  (projectile-mode 1)
   (setq projectile-completion-system 'helm)
   (setq projectile-switch-project-action
         (lambda ()
@@ -434,9 +431,6 @@
   :config
   (setq twittering-use-master-password t))
 
-(use-package vkill
-  :ensure t)
-
 (use-package web-mode
   :ensure t
   :mode ("\\.html\\'" . web-mode)
@@ -494,3 +488,28 @@
 (message "My emacs directory is: %s" user-emacs-directory)
 
 (setq debug-on-error nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans")))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote stack-ghci))
+ '(haskell-process-use-presentation-mode t)
+ '(haskell-tags-on-save t)
+ '(js-indent-level 2)
+ '(js2-auto-insert-catch-block nil)
+ '(js2-bounce-indent-p nil)
+ '(js2-mode-indent-ignore-first-tab nil)
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yatemplate yaml-mode xcscope wgrep web-mode use-package twittering-mode switch-window smartparens slime-company restclient rainbow-delimiters racket-mode racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode js2-refactor jenkins jasminejs-mode helm-swoop helm-projectile helm-flycheck helm-dash helm-company helm-ag haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed edit-server deft chicken-scheme cargo avy ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init/10-theme.el
+++ init/10-theme.el
@@ -9,7 +9,6 @@
 ;;;; Highlight entire expression within parens
 (show-paren-mode t)
 (setq show-paren-style 'expression)
-(set-face-background 'show-paren-match-face "#1f3f3f")
 
 ;;;; show the end
 (set-default 'indicate-empty-lines t)
