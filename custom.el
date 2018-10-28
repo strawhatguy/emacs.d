eb3c68d4dc6a4f98fba96df1c4bde7dc4607997b
Matthew Curry <mjcurry@gmail.com>
2018-10-28 01:47:55 -0400

    Move customizations to their own file

---
+++ custom.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans")))
+ '(haskell-process-auto-import-loaded-modules t)
+ '(haskell-process-log t)
+ '(haskell-process-suggest-remove-import-lines t)
+ '(haskell-process-type (quote stack-ghci))
+ '(haskell-process-use-presentation-mode t)
+ '(haskell-tags-on-save t)
+ '(js-indent-level 2)
+ '(js2-auto-insert-catch-block nil)
+ '(js2-bounce-indent-p nil)
+ '(js2-mode-indent-ignore-first-tab nil)
+ '(package-selected-packages
+   (quote
+    (projectile-ripgrep ripgrep slack zenburn-theme yatemplate yaml-mode xcscope wgrep web-mode use-package twittering-mode switch-window smartparens slime-company restclient rainbow-delimiters racket-mode racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode js2-refactor jenkins jasminejs-mode helm-swoop helm-projectile helm-flycheck helm-dash helm-company helm-ag haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed edit-server deft chicken-scheme cargo avy ag)))
+ '(recenter-positions (quote (middle))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
