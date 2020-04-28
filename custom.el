b67cfab96cbee56451dd5759fab8f3b4c4c6b0cc
Matthew Curry <mjcurry@gmail.com>
2020-04-28 00:01:42 -0400

    merge

--- custom.el
+++ custom.el
@@ -4,12 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-visited-interval 1)
- '(geiser-active-implementations (quote (guile chicken chez mit chibi)) t)
- '(haskell-process-args-cabal-repl (quote ("--ghc-option=-ferror-spans")))
+ '(geiser-active-implementations '(guile chicken chez mit chibi) t)
+ '(haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans"))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
  '(haskell-process-suggest-remove-import-lines t)
- '(haskell-process-type (quote stack-ghci))
+ '(haskell-process-type 'stack-ghci)
  '(haskell-process-use-presentation-mode t)
  '(haskell-tags-on-save t)
  '(js-indent-level 2)
@@ -18,9 +18,8 @@
  '(js2-mode-indent-ignore-first-tab nil)
  '(neo-window-width 50)
  '(package-selected-packages
-   (quote
-    (go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack zenburn-theme yatemplate yaml-mode xcscope wgrep web-mode use-package twittering-mode switch-window slime-company restclient rainbow-delimiters racket-mode racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode js2-refactor jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag)))
- '(recenter-positions (quote (middle)))
+   '(go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack zenburn-theme yatemplate yaml-mode xcscope wgrep web-mode use-package twittering-mode switch-window slime-company restclient rainbow-delimiters racket-mode racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode js2-refactor jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag))
+ '(recenter-positions '(middle))
  '(typescript-indent-level 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
