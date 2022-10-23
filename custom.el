b00da6b64722602292a16ef5874c0e21c4680d5d
Matthew Curry <mjcurry@gmail.com>
2022-10-23 14:39:55 -0700

    update eshell aliases

--- custom.el
+++ custom.el
@@ -4,7 +4,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-save-visited-interval 1)
- '(geiser-active-implementations '(guile chicken chez mit chibi) t nil "Customized with use-package geiser")
  '(haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans"))
  '(haskell-process-auto-import-loaded-modules t)
  '(haskell-process-log t)
@@ -26,7 +25,7 @@
  '(mc/always-run-for-all t)
  '(neo-window-width 50)
  '(package-selected-packages
-   '(unicode-fonts lsp-python-ms janet-mode vterm lastpass xkcd go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack yatemplate yaml-mode xcscope wgrep twittering-mode switch-window rainbow-delimiters racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag))
+   '(undo-tree unicode-fonts lsp-python-ms janet-mode vterm lastpass xkcd go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack yatemplate yaml-mode xcscope wgrep twittering-mode switch-window rainbow-delimiters racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag))
  '(recenter-positions '(middle))
  '(typescript-indent-level 2))
 (custom-set-faces
@@ -35,3 +34,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
