4f8d48f206ab54977d6c2bc5ef81f9cfa4e7a28b
Matthew Curry <mjcurry@gmail.com>
2022-10-23 21:01:23 -0700

    custom and .gitignore updates

--- custom.el
+++ custom.el
@@ -15,17 +15,10 @@
  '(js2-auto-insert-catch-block nil)
  '(js2-bounce-indent-p nil)
  '(js2-mode-indent-ignore-first-tab nil)
- '(lsp-file-watch-ignored
-   '("[/\\\\]\\.git$" "[/\\\\]\\.hg$" "[/\\\\]\\.bzr$" "[/\\\\]_darcs$" "[/\\\\]\\.svn$" "[/\\\\]_FOSSIL_$" "[/\\\\]\\.idea$" "[/\\\\]\\.ensime_cache$" "[/\\\\]\\.eunit$" "[/\\\\]node_modules$" "[/\\\\]\\.fslckout$" "[/\\\\]\\.tox$" "[/\\\\]dist$" "[/\\\\]dist-newstyle$" "[/\\\\]\\.stack-work$" "[/\\\\]\\.bloop$" "[/\\\\]\\.metals$" "[/\\\\]target$" "[/\\\\]\\.ccls-cache$" "[/\\\\]\\.vscode$" "[/\\\\]\\.deps$" "[/\\\\]build-aux$" "[/\\\\]autom4te.cache$" "[/\\\\]\\.reference$" "[/\\\\]vendor$"))
- '(lsp-file-watch-ignored-directories
-   '("[/\\\\]\\.git$" "[/\\\\]\\.hg$" "[/\\\\]\\.bzr$" "[/\\\\]_darcs$" "[/\\\\]\\.svn$" "[/\\\\]_FOSSIL_$" "[/\\\\]\\.idea$" "[/\\\\]\\.ensime_cache$" "[/\\\\]\\.eunit$" "[/\\\\]node_modules$" "[/\\\\]\\.fslckout$" "[/\\\\]\\.tox$" "[/\\\\]dist$" "[/\\\\]dist-newstyle$" "[/\\\\]\\.stack-work$" "[/\\\\]\\.bloop$" "[/\\\\]\\.metals$" "[/\\\\]target$" "[/\\\\]\\.ccls-cache$" "[/\\\\]\\.vscode$" "[/\\\\]\\.deps$" "[/\\\\]build-aux$" "[/\\\\]autom4te.cache$" "[/\\\\]\\.reference$" "[/\\\\]vendor$"))
- '(lsp-go-codelens nil)
- '(lsp-go-codelenses nil)
- '(lsp-gopls-codelens nil)
  '(mc/always-run-for-all t)
  '(neo-window-width 50)
  '(package-selected-packages
-   '(undo-tree unicode-fonts lsp-python-ms janet-mode vterm lastpass xkcd go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack yatemplate yaml-mode xcscope wgrep twittering-mode switch-window rainbow-delimiters racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag))
+   '(eglot undo-tree unicode-fonts lsp-python-ms janet-mode vterm lastpass xkcd go-playground go-dlv vue-html-mode vue-mode go-mode rjsx-mode diminish counsel-osx-app counsel-projectile counsel jest direnv dockerfile-mode typescript-mode parinfer spaceline dumb-jump fd-dired string-inflection edn deadgrep devdocs company-lsp lsp-ui lsp-clangd lsp-java lsp-mode exec-path-from-shell xref-js2 indium cider projectile-ripgrep ripgrep slack yatemplate yaml-mode xcscope wgrep twittering-mode switch-window rainbow-delimiters racer puppet-mode paredit nsis-mode nodejs-repl neotree magit json-mode jenkins jasminejs-mode haskell-mode google-translate google-this git-gutter geiser flycheck-rust expand-region elfeed deft chicken-scheme cargo avy ag))
  '(recenter-positions '(middle))
  '(typescript-indent-level 2))
 (custom-set-faces
